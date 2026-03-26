8367b20e57e92af2fdbfd58124000de5350905bf
ArghyaSharma <sharma.arghya614@gmail.com>
2026-03-26 17:30:32 +0530

    Change default port from 10000 to 8080

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ async def predict(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
